293ffd72fd7c7127084cc54457cfde9b64866570
robersan2212 <robertmolina1222@gmail.com>
2024-06-22 00:51:33 -0600

    created recipie site

---
+++ recipies/styles/main.js
@@ -0,0 +1,11 @@
+// 1. Direct Import
+import { functionName, objectName } from './recipies.mjs';
+
+// 2. Import Everything
+import * as Recipes from './recipies.mjs';
+
+// 3. Import with Alias
+import { functionName as newName } from './recipies.mjs';
+
+// 4. Default Import
+import DefaultExportName from './recipies.mjs';
